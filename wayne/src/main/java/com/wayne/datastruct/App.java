3524b160e6f7273a765dccafaa33dda382796e90
Wayne <w702897028970289@gmail.com>
2023-01-19 10:37:55 +0800

    second commit

--- wayne/src/main/java/com/wayne/datastruct/App.java
+++ wayne/src/main/java/com/wayne/datastruct/App.java
@@ -1,17 +1,11 @@
 package com.wayne.datastruct;
 
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int i = 0 ; i < 5 ; i++)
+            System.out.println("Hello World!");
     }
 }

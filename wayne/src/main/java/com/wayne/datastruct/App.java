6f8199f5cc908a844926b28bb97c088038731cdb
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2023-01-19 19:51:11 +0800

    update gitignore file

--- wayne/src/main/java/com/wayne/datastruct/App.java
+++ wayne/src/main/java/com/wayne/datastruct/App.java
@@ -4,8 +4,6 @@ import com.wayne.datastruct.array.MyArrayList;
 import com.wayne.datastruct.linkedList.MyLinkedList;
 
 public final class App {
-    private App() {
-    }
 
     public static void main(String[] args) {
         MyLinkedList list = new MyLinkedList(4);

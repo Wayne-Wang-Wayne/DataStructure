17a28004911953ec8f47f16c688180c188644485
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2023-02-11 11:15:21 +0800

    Queue OK

--- wayne/src/main/java/com/wayne/datastruct/App.java
+++ wayne/src/main/java/com/wayne/datastruct/App.java
@@ -5,14 +5,24 @@ import com.wayne.datastruct.doublyLinkedList.CircularDoublyLinkedList;
 import com.wayne.datastruct.doublyLinkedList.DoublyLinkedList;
 import com.wayne.datastruct.linkedList.CircularLinkedList;
 import com.wayne.datastruct.linkedList.MyLinkedList;
+import com.wayne.datastruct.queue.MyQueue;
 import com.wayne.datastruct.stack.CustomCharStack;
 import com.wayne.datastruct.stack.MyStack;
 
 public final class App {
 
     public static void main(String[] args) {
-        CustomCharStack stack = new CustomCharStack();
-        int result = stack.calculate("10*(3+5)-10-20");
-        System.out.println(result);
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.enqueue(8);
+        queue.enqueue(9);
+        queue.dequeue();
+        queue.display();
     }
 }

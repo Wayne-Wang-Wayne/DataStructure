66516f9a3e6283584da73944d9cf4dfd0ac1c918
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2023-03-11 16:04:03 +0800

    bubble sort ok

--- wayne/src/main/java/com/wayne/datastruct/App.java
+++ wayne/src/main/java/com/wayne/datastruct/App.java
@@ -9,6 +9,7 @@ import com.wayne.datastruct.linkedList.MyLinkedList;
 import com.wayne.datastruct.queue.DEQueue;
 import com.wayne.datastruct.queue.MyQueue;
 import com.wayne.datastruct.queue.PriorityQueue;
+import com.wayne.datastruct.sortUtils.SortUtils;
 import com.wayne.datastruct.stack.CustomCharStack;
 import com.wayne.datastruct.stack.MyStack;
 import com.wayne.datastruct.tree.AVLTree;
@@ -18,19 +19,8 @@ import com.wayne.datastruct.tree.MyTree;
 public final class App {
 
     public static void main(String[] args) {
-        MyHeap heap = new MyHeap();
-        heap.insert(10);
-        heap.insert(20);
-        heap.insert(30);
-        heap.insert(25);
-        heap.insert(5);
-        heap.insert(40);
-        heap.insert(35);
-        heap.delete();
-        heap.print();
-        int[] sortedArray = heap.heapSortTheRest();
-        for(int i = 0 ; i < sortedArray.length ; i++) {
-            System.out.print(sortedArray[i] + " ");
-        }
+        int[] array = new int[]{3,7,9,10,6,5,12,4,11,2};
+        SortUtils.bubbleSort(array);
+        SortUtils.printList(array);
     }
 }
---
+++ wayne/src/main/java/com/wayne/datastruct/sortUtils/SortUtils.java
@@ -0,0 +1,30 @@
+package com.wayne.datastruct.sortUtils;
+
+public class SortUtils {
+    
+    // min: O(n)
+    // max: O(n^2)
+    public static void bubbleSort(int[] list) {
+        boolean hasSwap = false;
+        for(int i = 0 ; i < list.length - 1 ; i++) {
+            hasSwap = false;
+            for(int j = 0 ; j < list.length-1-i ; j++) {
+                if(list[j] > list[j+1]) {
+                    // swap
+                    int temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
+                    hasSwap = true;
+                }
+            }
+            // if no change made in current loop means it was sorted
+            if(!hasSwap) break;
+        }
+    }
+
+    public static void printList(int[] list) {
+        for(int i : list) {
+            System.out.print(i + " ");
+        }
+    }
+}

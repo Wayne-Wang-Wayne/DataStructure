60c5945042e1a37a33e7bf273c3692c464779272
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2023-03-11 16:47:25 +0800

    clean up import file

--- wayne/src/main/java/com/wayne/datastruct/App.java
+++ wayne/src/main/java/com/wayne/datastruct/App.java
@@ -1,20 +1,6 @@
 package com.wayne.datastruct;
 
-import com.wayne.datastruct.array.MyArrayList;
-import com.wayne.datastruct.doublyLinkedList.CircularDoublyLinkedList;
-import com.wayne.datastruct.doublyLinkedList.DoublyLinkedList;
-import com.wayne.datastruct.heap.MyHeap;
-import com.wayne.datastruct.linkedList.CircularLinkedList;
-import com.wayne.datastruct.linkedList.MyLinkedList;
-import com.wayne.datastruct.queue.DEQueue;
-import com.wayne.datastruct.queue.MyQueue;
-import com.wayne.datastruct.queue.PriorityQueue;
 import com.wayne.datastruct.sortUtils.SortUtils;
-import com.wayne.datastruct.stack.CustomCharStack;
-import com.wayne.datastruct.stack.MyStack;
-import com.wayne.datastruct.tree.AVLTree;
-import com.wayne.datastruct.tree.BSTree;
-import com.wayne.datastruct.tree.MyTree;
 
 public final class App {
 
